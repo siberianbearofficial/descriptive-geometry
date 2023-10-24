8b79d07534d85b561c58586e01ff536c9cd87feb
Aleksei Orlov <alexeyorlov65@gmail.com>
2023-10-24 17:32:08 +0300

    Frontend Plot component

---
+++ frontend/src/app/presentation/shared/components/plot/plot.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plot',
+  templateUrl: './plot.component.html',
+  styleUrls: ['./plot.component.scss']
+})
+export class PlotComponent {
+
+}
