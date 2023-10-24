d326cdeb569bac5b84973db89a8b241db122f9e5
Aleksei Orlov <alexeyorlov65@gmail.com>
2023-10-24 17:31:52 +0300

    Frontend project settings

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import { ProjectPageComponent } from './presentation/pages/project-page/project-page.component';
+import { PlotComponent } from './presentation/shared/components/plot/plot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectPageComponent
+    ProjectPageComponent,
+    PlotComponent
   ],
   imports: [
     BrowserModule,
